07dde21aefbccc42f3dd08e3dcc83374c45accbc
Fedor <fedor_prg@rambler.ru>
2022-05-22 20:32:53 +1000

    Updated Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,20 +1,26 @@
-﻿void Increase(int a, int b)
-{    
+﻿void Numbers(int a, int b)
+{
     if (a == b)
-        return;    
-    Console.Write(a + 1 + ", ");    
-    Increase(a+1, b);    
-}
-void Descend(int a, int b)
-{    
-    if (a == b)
-        return;    
-    Console.Write(a-1 + ", ");
-    Descend(a-1, b);   
+    {
+        Console.Write(a);
+        return;
+    }
+    else
+    {
+        if (a < b)
+        {
+            Console.Write(a + ", ");
+            Numbers(a + 1, b);
+        }
+        else
+        {
+            Console.Write(a + ", ");
+            Numbers(a - 1, b);
+        }
+    }
 }
 Console.Write("Введите число M: ");
 int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine()!);
-if (m<n) Increase(m-1, n);
-else Descend(m+1, n);
+Numbers(m, n);
