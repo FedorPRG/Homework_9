7cdf55b24387f1d07003e64d22eded98abc0d17f
Fedor <fedor_prg@rambler.ru>
2022-05-22 14:32:59 +1000

    Created Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿void Increase(int a, int b)
+{    
+    if (a == b)
+        return;    
+    Console.Write(a + 1 + ", ");    
+    Increase(a+1, b);    
+}
+void Descend(int a, int b)
+{    
+    if (a == b)
+        return;    
+    Console.Write(a-1 + ", ");
+    Descend(a-1, b);   
+}
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+if (m<n) Increase(m-1, n);
+else Descend(m+1, n);
---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int Summa(int a, int b)
+{
+    if (a == b)
+        return a;
+    return a + Summa(a + 1, b);
+}
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Summa(m, n));
